cf50b880e612c4917b20fc78e7ce45b060d5a905
cdelattre08 <cyril.delattre02@gmail.com>
2023-05-02 12:16:37 +0200

    Petits ajustements

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById('burger-menu').addEventListener('click', function() {
     document.querySelector('.topnav').classList.toggle('menu-open');
 });
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
